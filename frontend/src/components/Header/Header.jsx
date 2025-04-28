9c77184643a25fd52677eda473327c0f12c97184
Shiba Murmu <shibamurmu001@gmail.com>
2025-04-28 22:29:10 +0530

    some header profile image is changed.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -196,7 +196,7 @@ function Header() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="./images/myimage.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
