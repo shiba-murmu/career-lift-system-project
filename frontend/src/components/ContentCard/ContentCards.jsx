376f860292e552ce2cb22abc7d002acff292de43
Shiba Murmu <shibamurmu001@gmail.com>
2025-05-03 15:11:49 +0530

    refactor(ContentCards): adjust layout and font sizes for improved readability and consistency.

--- frontend/src/components/ContentCard/ContentCards.jsx
+++ frontend/src/components/ContentCard/ContentCards.jsx
@@ -22,46 +22,46 @@ function ContentCards() {
       {/* Main Card Container */}
 
       <div
-        className="hidden md:block  bg-gray-300 dark:border-gray-500 rounded-lg dark:bg-neutral-800 shadow-2xl dark:shadow-0 p-5 h-[27rem] w-100% md:w-[25%]"
+        className="hidden md:block  bg-gray-300 dark:border-gray-500 rounded-lg dark:bg-neutral-800 shadow-2xl dark:shadow-0 p-5 h-[20.5rem] w-100% md:w-[25%]"
         style={{ fontFamily: "Open sans , " }}
       >
         {/* Bookmark Button */}
-        <div className="flex justify-end items-center mb-4">
+        <div className="flex justify-end items-center ">
           <button className="relative" onClick={toggleBookmark}>
             {isBookmarked ? <TurnedInIcon /> : <TurnedInNotIcon />}
           </button>
         </div>
 
         {/* Card Header */}
-        <div>
-          <span className="text-2xl text-blue-800 font-bold dark:text-blue-300">
+        <div className="flex justify-start items-center mb-1">
+          <span className="text-xl text-blue-800 font-bold dark:text-blue-300">
             UX/UI Designer
           </span>
         </div>
 
         {/* Job Details Section */}
-        <div className="flex justify-between items-center mt-4 mb-4">
+        <div className="flex justify-between items-center mt-1 mb-1">
           {/* Experience */}
           <div>
             <PeopleOutlineIcon fontSize="small" />
-            <span className="text-sm font-bold">1 - 2 Years</span>
+            <span className="text-[4px] font-bold ml-1">1 - 2 Years</span>
           </div>
 
           {/* Job Type */}
           <div className="flex items-center gap-2">
             <AccessTimeIcon fontSize="small" />
-            <span className="text-sm font-bold">Full Time</span>
+            <span className="text-[4px] font-bold">Full Time</span>
           </div>
 
           <div>
             <LocationPinIcon fontSize="small" />
-            <span className="text-sm font-bold">Remote</span>
+            <span className="text-[4px] font-bold">Remote</span>
           </div>
         </div>
 
         {/* Job Description */}
         <div>
-          <p className="text-sm text-gray-600 dark:text-gray-300">
+          <p className="text-[4px] text-gray-600 dark:text-gray-300">
             We are looking for a creative and detail-oriented UX/UI Designer to
             join our team. The ideal candidate will have experience in designing
             user-friendly interfaces and a strong understanding of design
@@ -70,15 +70,15 @@ function ContentCards() {
         </div>
 
         {/* Posted Date */}
-        <div className="flex justify-between items-center mt-4">
+        <div className="flex justify-between items-center mt-2">
           <div className="flex items-center gap-2">
             <AccessAlarmIcon fontSize="small" />
-            <span className="text-sm font-thin">Posted on: Oct 10, 2023</span>
+            <span className="text-[4px] font-thin text-amber-400">Posted on: Oct 10, 2023</span>
           </div>
         </div>
 
         {/* Skills Section */}
-        <div className="flex justify-between text-sm items-center mt-4 mb-4">
+        <div className="flex justify-between text-[4px] items-center mt-4 mb-4">
           <div className="flex items-center bg-fuchsia-400 dark:bg-neutral-700 p-1 px-2 rounded-2xl font-light">
             Javascript
           </div>
@@ -95,8 +95,8 @@ function ContentCards() {
 
         {/* View Details Button */}
         <div>
-          <button className="bg-amber-800 dark:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full flex items-center justify-center hover:bg-amber-700 dark:hover:bg-blue-400 transition duration-300 ease-in-out">
-            <span className="text-sm text-gray-200 dark:text-gray-300 font-bold">
+          <button className="bg-amber-800 dark:bg-blue-900 text-white font-bold py-2 px-4 rounded-full w-full flex items-center justify-center hover:bg-amber-700 dark:hover:bg-blue-700 transition duration-300 ease-in-out">
+            <span className="text-[4px] text-gray-200 dark:text-gray-300 font-bold">
               View Details
             </span>
           </button>
