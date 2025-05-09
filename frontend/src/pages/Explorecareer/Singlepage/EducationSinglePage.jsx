61be4553e2161fc3917cef09f958f6b3542e44ce
Shiba Murmu <shibamurmu001@gmail.com>
2025-05-09 14:23:14 +0530

    fix(EducationSinglePage): format code for consistency and readability

--- frontend/src/pages/Explorecareer/Singlepage/EducationSinglePage.jsx
+++ frontend/src/pages/Explorecareer/Singlepage/EducationSinglePage.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 function EducationSinglePage() {
-    const { id, postId } = useParams()
-    return (
-        <div>
-            <h1>Education Details for ID: {id} and Post ID: {postId}</h1>
-        </div>
-    )
+  const { id, postId } = useParams();
+  return (
+    <>
+      <h1>
+        Education Details for ID: {id} and Post ID: {postId}
+      </h1>
+    </>
+  );
 }
 
-export default EducationSinglePage
+export default EducationSinglePage;
