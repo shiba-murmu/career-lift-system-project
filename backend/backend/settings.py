01e72fd0e93ac19d42c21a214c04275f142ec7c6
Shiba Murmu <shibamurmu001@gmail.com>
2025-06-18 20:43:07 +0530

    makes changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ CORS_ALLOW_ALL_ORIGINS = True # for development only, use with caution in produc
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React local dev server
-    "https://career-lift-system.vercel.app/" # For production
+    # "https://career-lift-system.vercel.app/" # For production
     # Add production frontend URL later
 ]
 # Optional but helpful:
@@ -105,11 +105,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'default' : dj_database_url.config(default=config('DATABASE_URL'))
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
