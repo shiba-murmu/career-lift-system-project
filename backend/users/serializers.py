1f4e571446f4feac099d35f8775839019bb540e7
Shiba Murmu <shibamurmu001@gmail.com>
2025-06-19 07:47:51 +0530

    my serializer changed

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -30,6 +30,32 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
             'username': user.username,
         }
 
+# class RegisterSerializer(serializers.ModelSerializer):
+#     confirm_password = serializers.CharField(write_only=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['email', 'first_name', 'last_name', 'password', 'confirm_password']
+#         extra_kwargs = {
+#             'password': {'write_only': True},
+#             'email': {'required': True}
+#         }
+
+#     def validate(self, data):
+#         if data['password'] != data['confirm_password']:
+#             raise serializers.ValidationError({"password": "Passwords do not match"})
+#         return data
+
+#     def create(self, validated_data):
+#         validated_data.pop('confirm_password')
+#         validated_data['username'] = validated_data['email']  # ✅ Set username as email
+#         validated_data['password'] = make_password(validated_data['password'])
+#         return User.objects.create(**validated_data)
+
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+# from django.contrib.auth.hashers import make_password
+
 class RegisterSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
 
@@ -38,16 +64,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['email', 'first_name', 'last_name', 'password', 'confirm_password']
         extra_kwargs = {
             'password': {'write_only': True},
-            'email': {'required': True}
+            'email': {'required': True},
         }
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError({"password": "Passwords do not match"})
+            raise serializers.ValidationError({"confirm_password": "Passwords do not match"})
         return data
 
     def create(self, validated_data):
         validated_data.pop('confirm_password')
-        validated_data['username'] = validated_data['email']  # ✅ Set username as email
-        validated_data['password'] = make_password(validated_data['password'])
+        validated_data['username'] = validated_data['email']  # Django requires username
+        validated_data['password'] = make_password(validated_data['password'])  # hash password
         return User.objects.create(**validated_data)
