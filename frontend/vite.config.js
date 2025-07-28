30090ec348a83b5e3875f379a593ae63f96a23e2
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-28 07:56:26 +0530

    code updated now i can see this webapp to my phone live server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // or '0.0.0.0' to expose to the network
+    port: 5173, // default Vite port
+  },
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // adjust based on our project
   darkMode: "class",
   plugins: [tailwindcss(), react()],
 });
-
